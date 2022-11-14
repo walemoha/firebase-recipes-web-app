440a1f1eb155f6b8f013777287a211b636e57993
waleed Mohammed <waleedmohammed@waleeds-MacBook-Pro.local>
2022-11-14 05:07:58 +0300

    initial commit

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import {  initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyBgVNYBqOvm9i-7MN1ucleDmsomIQjXhRM",
+  authDomain: "fir-recipes-3315e.firebaseapp.com",
+  projectId: "fir-recipes-3315e",
+  storageBucket: "fir-recipes-3315e.appspot.com",
+  messagingSenderId: "563906369948",
+  appId: "1:563906369948:web:8c3f97eb4b255a91019059",
+  measurementId: "G-H1VK3KEQJ1"
+});
+
+// Initialize Firebase
+
+
+export default firebaseConfig
